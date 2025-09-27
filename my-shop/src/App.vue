c6f2fc7feebdf3360d5c63d3a96c16a705c2c937
spongesqueeze <vladgorbeiko@gmail.com>
2025-09-27 19:09:31 +0300

    update

--- my-shop/src/App.vue
+++ my-shop/src/App.vue
@@ -5,17 +5,21 @@
     description="Стильна вишиванка з традиційними орнаментами."
     price="1500"
     image="https://content1.rozetka.com.ua/goods/images/big/383454333.jpg"/>
+    <ReviewsComponent/>
     <FooterComponent />
+  
   </div>
 </template>
 
 <script>
 import ProductList from './components/ProductList.vue';
+import ReviewsComponent from './components/ReviewsComponent.vue';
 import FooterComponent from './components/FooterComponent.vue';
 
 export default{
   components:{
     ProductList,
+    ReviewsComponent,
     FooterComponent
   }
 };
